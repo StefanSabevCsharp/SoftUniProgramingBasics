c9b5f2b6700cb1c2cc90cdc161a957310d369df2
StefanSabevCsharp <stevenbg91@gmail.com>
2022-11-06 14:02:43 +0100

    Add files via upload
    Simple Exercises from SoftUni

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double hours = int.Parse(Console.ReadLine());
+            double minutes = int.Parse(Console.ReadLine());
+
+            minutes += 15;
+            if (minutes >= 45)
+            {
+                hours += 1;
+            }
+            if (minutes > 60)
+            {
+                minutes = minutes - 60;
+            }
+            //Console.WriteLine($"{hours}:{minutes}");
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+            Console.WriteLine($"{hours}:{minutes}");
+        }
+    }
+}
