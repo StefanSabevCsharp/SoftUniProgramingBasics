fcc0e099d96648d2c4650e81e36c04f7874549c4
Stefan Sabev <stevenbg91@gmail.com>
2022-11-09 21:07:56 +0100

    Add files via upload

---
+++ BirthdayParty/BirthdayParty/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BirthdayParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rentforHall = double.Parse(Console.ReadLine());
+            double cake = rentforHall * 0.2;
+            double drinks = cake - (cake * 0.45);
+            double animator = rentforHall / 3;
+            double totalsum = rentforHall + cake + drinks + animator;
+            Console.WriteLine(totalsum);
+//            •	Торта  – цената ѝ е 20 % от наема на залата
+//•	Напитки – цената им е 45 % по - малко от тази на тортата
+//•	Аниматор – цената му е 1 / 3 от цената за наема на залата
+
+        }
+    }
+}
