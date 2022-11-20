318f5e1c7a4006b9d57695b97e6896b5b5240643
Stefan Sabev <stevenbg91@gmail.com>
2022-11-20 22:17:04 +0100

    Add files via upload

---
+++ InchToSM/InchToSM/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InchToSM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Да се напише програма, 
+            //    която чете от конзолата число(реално число) и
+            //    преобразува числото от инчове в сантиметри.
+            //    За целта умножава инчовете по 2.54(защото 1 инч = 2.54 сантиметра).
+            double chete = double.Parse(Console.ReadLine  ());
+            Console.WriteLine(chete * 2.54);
+
+        }
+
+    }
+}
