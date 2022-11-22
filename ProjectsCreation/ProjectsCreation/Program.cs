3a66d6993ce6c75ead01e6062013bbd02dfa33bf
Stefan Sabev <stevenbg91@gmail.com>
2022-11-22 21:37:22 +0100

    Add files via upload

---
+++ ProjectsCreation/ProjectsCreation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProjectsCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int chasove = 3;
+                int proekti = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The architect {name} will need {proekti * chasove } hours to complete {proekti} project/s.");
+        }
+        
+    }
+}
---
+++ RadianstoDegrees/RadianstoDegrees/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RadianstoDegrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rad = double.Parse(Console.ReadLine());
+            double gradus = rad * 180 / Math.PI;
+            Console.WriteLine(Math.Round(gradus));
+        }
+    }
+}
